5f18280f11b640cbee33c0c5dc7ec96df60fccf6
dimitardimitrov05 <109605825+dimitardimitrov05@users.noreply.github.com>
2024-07-16 14:49:22 +0300

    creted project and added gitignore file

---
+++ Social Network Project/Connectly/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Connectly.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
